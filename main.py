be45c302cbe703fabd0d6e0270402d2c11f8dddf
Diego Antonino <diego_antonino@hotmail.com>
2018-07-06 20:54:20 -0400

    fix equal

--- main.py
+++ main.py
@@ -5,7 +5,10 @@ retry_num = 5
 retry_min_msec = 200
 
 try:
-    light_sensor = tsl2561.TSL2561()
+    #light_sensor = tsl2561.TSL2561()
+    print("waiting 10 seconds to stop the main")
+    utime.sleep(10)
+    pass
 except Exception as e:
     print("Failed to initialize Light Sensor. Error: {}".format(e))
     tools.led_error()
@@ -14,7 +17,15 @@ else:
     while True:
         lux = 0
         try:
-            lux = light_sensor.get_lux()
+            #lux = light_sensor.get_lux()
+
+            ####### workaround until i get the sensor
+            time = utime.localtime()
+            if 0 <= time[3] <= 11:  # from 20hs to 7am hs EST. localtime gives UTC
+                lux = 0
+            else:
+                lux = 1000
+            ###################################
         except Exception as e:
             print("Failed to get luminosity. Error: {}".format(e))
             tools.led_error()
@@ -30,7 +41,11 @@ else:
             if retry >= retry_num and send_failed:
                 print("Tried: {} times and it couldn't send values.".format(retry))
         finally:
-            if 200 < lux < 1000:
-                utime.sleep(5)
-            else:
-                utime.sleep(300)
+            #if 200 < lux < 1000:
+            #    utime.sleep(5)
+            #else:
+            #    utime.sleep(300)
+
+            ####### workaround until i get the sensor
+            utime.sleep(300)
+            ###################################
